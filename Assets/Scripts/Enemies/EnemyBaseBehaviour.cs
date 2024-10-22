ea6005c64d01771dcb1dd9f1b87af5fcc7e1781e
Oscar Devs <ohernandezba@gmail.com>
2024-10-22 16:38:57 +0200

    enemy movement

--- Assets/Scripts/Enemies/EnemyBaseBehaviour.cs
+++ Assets/Scripts/Enemies/EnemyBaseBehaviour.cs
@@ -4,36 +4,13 @@ using Synty.AnimationBaseLocomotion.Samples;
 using Synty.AnimationBaseLocomotion.Samples.InputSystem;
 using UnityEngine;
 
-[RequireComponent(typeof(InputReader))]
-[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(EnemyController))]
 public class EnemyBaseBehaviour : MonoBehaviour
 {
-    protected InputReader _inputReader;
-    protected PlayerController _playerController;
+    protected EnemyController _enemyController;
 
     protected virtual void Awake()
     {
-        _inputReader = GetComponent<InputReader>();
-        _playerController = GetComponent<PlayerController>();
-        StartCoroutine(PruebaCouRotine());
-    }
-
-    IEnumerator PruebaCouRotine()
-    {
-        while (true)
-        {
-            //_playerController.IsStopped = false;
-            //_playerController.IsGrounded = true;
-            //_playerController.IsStarting = false;
-            //_playerController.MovementInputTapped = false;
-            //_playerController.MovementInputHeld = true;
-            _inputReader._moveComposite = new Vector2(0, 1f);
-            _inputReader._movementInputDetected = true;
-            yield return new WaitForSeconds(0.1f);
-            _inputReader._movementInputDetected = false;
-
-            yield return new WaitUntil(() => _playerController.IsStopped && _playerController.IsGrounded && !_playerController.MovementInputTapped && !_playerController.MovementInputHeld);
-            yield return new WaitForSeconds(0.3f);
-        }
+        _enemyController = GetComponent<EnemyController>();
     }
 }
--- Assets/Scripts/Enemies/EnemySimpleBehaviour.cs
+++ Assets/Scripts/Enemies/EnemySimpleBehaviour.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class EnemySimpleBehaviour : EnemyBaseBehaviour
 {
-
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        _enemyController.StartMove(Vector2.up);
+        yield return new WaitForSeconds(3f);
+        _enemyController.StopMove();
+        _enemyController.Rotate(Vector2.right);
+        _enemyController.StartMove(Vector2.down);
+        yield return new WaitForSeconds(3f);
+        _enemyController.StopMove();
+    }
 }
